9f7e18cbaafbb4c8777e860df14670e6634a572d
Lemacoder <ladygin.anastasia@yandex.ru>
2025-03-11 19:42:32 +0300

    tasks and populate places

--- ASPBE/UserMainMenu/views.py
+++ ASPBE/UserMainMenu/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from HoldingMainMenu.models import Venues, Photos, UserAction
 import json
+from django.db.models import Count
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 # Create your views here.
 
-
-
+ACTIVE = 1
 
 @csrf_exempt
 @require_http_methods(["POST"])
@@ -34,9 +34,21 @@ def save_user_data(request):
 
 
 def mainpage(request):
-    posts = Venues.objects.filter(status=1)
-    photos = Photos.objects.all()
-    return render(request, 'Users/usermain.html', {'photos': photos, 'posts': posts})
+    #ACTIVE = 1 обработать !!!!!!!!!!!!!!!!!!!!!!!!!!
+    posts = Venues.objects.filter(status=ACTIVE)
+
+    popular_venues = (
+        UserAction.objects.filter(action_type__in=['Наведение', 'Нажатие'])
+        .values('venue') 
+        .annotate(total_actions=Count('id'))  
+        .order_by('-total_actions')[:10]  
+    )
+
+    
+    popular_venue_ids = [entry['venue'] for entry in popular_venues]
+    popular_venues_details = Venues.objects.filter(id__in=popular_venue_ids)
+
+    return render(request, 'Users/usermain.html', {'posts': posts, 'popular_venues_details':popular_venues_details})
 
 
 def placepage(request, venid):
